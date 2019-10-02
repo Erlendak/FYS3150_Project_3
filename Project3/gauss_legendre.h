206d8e298006e6b6ba9b2167a18ea3ca4691803f
roaz1801 <54395470+roaz1801@users.noreply.github.com>
2019-10-02 21:14:33 +0200

    Added function that is going to be integrated.

---
+++ Project3/gauss_legendre.h
@@ -0,0 +1,25 @@
+#ifndef GAUSS_LEGENDRE_H
+#define GAUSS_LEGENDRE_H
+
+#endif // GAUSS_LEGENDRE_H
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+#include "armadillo"
+
+using namespace arma;
+using namespace std;
+
+double func(double x1,double y1, double z1, double x2, double y2, double z2){
+    double alpha = 2.0;
+
+    double exp1 = -2*alpha*sqrt((x1*x1)+(y1*y1)+(z1*z1));
+    double exp2 = -2*alpha*sqrt((x2*x2)+(y2*y2)+(z2*z2));
+    double deno = sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2));
+
+    return exp(exp1+exp2)/deno;
+}
