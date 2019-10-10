ac46499feceae2bef4c3e1949968b8c0f23bb92e
Erlendak <Erlendak@mail.uio.no>
2019-10-10 11:48:21 +0200

    Correction of the Gauss Laguerre method, still needs a bit of work to go as intended.

--- Project3/gauss_lag.h
+++ Project3/gauss_lag.h
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <cmath>
 #include <stdlib.h>
 #include <stdio.h>
 #define EPS 3.0e-14
@@ -14,7 +13,22 @@
 
 using namespace std;
 
-double gammln(double);
+
+double gammln( double xx)
+{
+    double x,y,tmp,ser;
+    static double cof[6]={76.18009172947146,-86.50532032941677,
+        24.01409824083091,-1.231739572450155,
+        0.1208650973866179e-2,-0.5395239384953e-5};
+    int j;
+
+    y=x=xx;
+    tmp=x+5.5;
+    tmp -= (x+0.5)*log(tmp);
+    ser=1.000000000190015;
+    for (j=0;j<=5;j++) ser += cof[j]/++y;
+    return -tmp+log(2.5066282746310005*ser/x);
+}
 
 //  Note that you need to call it with a given value of alpha,
 // called alf here. This comes from x^{alpha} exp(-x)
@@ -55,22 +69,6 @@ void gauss_laguerre(double *x, double *w, int n, double alf)
 }
 // end function gaulag
 
-double gammln( double xx)
-{
-    double x,y,tmp,ser;
-    static double cof[6]={76.18009172947146,-86.50532032941677,
-        24.01409824083091,-1.231739572450155,
-        0.1208650973866179e-2,-0.5395239384953e-5};
-    int j;
-
-    y=x=xx;
-    tmp=x+5.5;
-    tmp -= (x+0.5)*log(tmp);
-    ser=1.000000000190015;
-    for (j=0;j<=5;j++) ser += cof[j]/++y;
-    return -tmp+log(2.5066282746310005*ser/x);
-}
-
 // end function gammln
 #undef EPS
 #undef MAXIT
@@ -84,16 +82,19 @@ double func_spherical(double r1,double theta1, double phi1, double r2, double th
 
     double alpha = 2.0;
     double tol = 1E-10; //Tolerance: |r1-r2|=0 can cause problems, so have to check.
-    double deno = sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2));
+    //double deno = sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2));
 
     //We make an if test, because we have to account for when deno goes towards zero,
     //if we don't, our expression will go against infinity.
+    double beta = cos(theta1)*cos(theta2)+sin(theta1)*sin(theta2)*cos(phi1-phi2);
+    double deno = sqrt((r1*r1) + (r2*r2) - 2*r1*r2*beta);
     if(deno < tol){
         return 0;
-    }else{
-        double exp1 = -2*alpha*sqrt((x1*x1)+(y1*y1)+(z1*z1));
-        double exp2 = -2*alpha*sqrt((x2*x2)+(y2*y2)+(z2*z2));
-        return exp(exp1+exp2)/deno;
+    }
+    else{
+    double exp1 = -2*alpha*sqrt(r1);
+    double exp2 = -2*alpha*sqrt(r2);
+    return exp(exp1+exp2)/deno;
     }
 }
 
--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 #include <gauss_legendre.h>
 #include <gauss_lag.h>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
 //task3a();
-
-int n = 4;
-double a = 0;
-double b= 1;
+double pi = 3.14159265;
+int N = 10;
 //   reserve space in memory for vectors containing the mesh points
 //   weights and function values for the use of the gauss-legendre
 //   method
 // Gauss-Laguerre is old-fashioned translation of F77 --> C++
 // arrays start at 1 and end at n
-double *xgl = new double [n+1];
-double *wgl = new double [n+1];
+double *xgl = new double [N+1];
+double *wgl = new double [N+1];
         // These arrays are used for improved Gauss-Legendre, mapping of
         // x \in [-1,1] to x \in [0, infinity)
 
    //   set up the mesh points and weights
    //   set up the mesh points and weights and the power of x^alf
 double alf = 2.0;
-gauss_laguerre(xgl,wgl, n, alf);
+gauss_laguerre(xgl,wgl, N, alf);
 
 //   evaluate the integral with the Gauss-Laguerre method
 //   Note that we initialize the sum
-double int_gausslag = 0.;
-for ( int i = 1;  i <= n; i++){
-    int_gausslag += wgl[i]*sin(xgl[i]);
-}
+double int_gausslag = 0.0;
+for (int i=0;i<N;i++){
+    cout << i << endl;
+    for (int j = 0; j<N; j++){
+    for (int k = 0; k<pi; k++){
+    for (int l = 0; l<pi; l++){
+    for (int m = 0; m< 2*pi; m++){
+    for (int n = 0; n < 2*pi; n++){
+
+    int_gausslag += wgl[i]*wgl[k]*wgl[m]*wgl[j]*wgl[l]*wgl[n]
+            *func_spherical(xgl[i],xgl[k],xgl[m],xgl[j],xgl[l],xgl[n]);
+
+//cout<< int_gausslag<<endl;
+}}}}}}
+delete []xgl;
+delete []wgl;
 cout<< int_gausslag<<endl;
+return 0;
 }
