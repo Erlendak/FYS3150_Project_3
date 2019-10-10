63f074e9828489734857ca82dc1fd638d1010082
erlendak <erlendak@mail.uio.no>
2019-10-10 10:03:51 +0200

    Further development on gauss_laguerre method

--- Project3/gauss_lag.h
+++ Project3/gauss_lag.h
@@ -1,3 +1,7 @@
+#ifndef GAUSS_LAG_H
+#define GAUSS_LAG_H
+
+
 #include <cmath>
 #include <iostream>
 #include <fstream>
@@ -70,3 +74,29 @@ double gammln( double xx)
 // end function gammln
 #undef EPS
 #undef MAXIT
+
+
+
+double func_spherical(double r1,double theta1, double phi1, double r2, double theta2, double phi2){
+    /*func(double x1,double y1, double z1, double x2, double y2, double z2):
+     * Function that sets up the integral we're going to integrate.
+      This is a six-dimensional integral.*/
+
+    double alpha = 2.0;
+    double tol = 1E-10; //Tolerance: |r1-r2|=0 can cause problems, so have to check.
+    double deno = sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2));
+
+    //We make an if test, because we have to account for when deno goes towards zero,
+    //if we don't, our expression will go against infinity.
+    if(deno < tol){
+        return 0;
+    }else{
+        double exp1 = -2*alpha*sqrt((x1*x1)+(y1*y1)+(z1*z1));
+        double exp2 = -2*alpha*sqrt((x2*x2)+(y2*y2)+(z2*z2));
+        return exp(exp1+exp2)/deno;
+    }
+}
+
+
+
+#endif // GAUSS_LAG_H
--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <gauss_legendre.h>
-#include <lib.h>
+#include <gauss_lag.h>
 
 using namespace std;
 
 int main()
 {
-    int N = 27;
-    int a = -3;
-    int b = 3;
-    double *x = new double [N];
-    double *w = new double [N];
-//  Function from lib, sets up mesh grid and weights.
-    gauleg(a,b,x,w, N);
+//task3a();
 
-//   Gauss-Legendre method
-    double gauss = 0.0;
-    for (int i=0;i<N;i++){
-        cout << i << endl;
-        for (int j = 0;j<N;j++){
-        for (int k = 0;k<N;k++){
-        for (int l = 0;l<N;l++){
-        for (int m = 0;m<N;m++){
-        for (int n = 0;n<N;n++){
-       gauss+=w[i]*w[j]*w[k]*w[l]*w[m]*w[n]*func_cartesian(x[i],x[j],x[k],x[l],x[m],x[n]);
-           }}}}}
-    }
-    cout << gauss<< endl;
+int n = 4;
+double a = 0;
+double b= 1;
+//   reserve space in memory for vectors containing the mesh points
+//   weights and function values for the use of the gauss-legendre
+//   method
+// Gauss-Laguerre is old-fashioned translation of F77 --> C++
+// arrays start at 1 and end at n
+double *xgl = new double [n+1];
+double *wgl = new double [n+1];
+        // These arrays are used for improved Gauss-Legendre, mapping of
+        // x \in [-1,1] to x \in [0, infinity)
 
-    delete [] x;
-    delete [] w;
-    return 0;
+   //   set up the mesh points and weights
+   //   set up the mesh points and weights and the power of x^alf
+double alf = 2.0;
+gauss_laguerre(xgl,wgl, n, alf);
+
+//   evaluate the integral with the Gauss-Laguerre method
+//   Note that we initialize the sum
+double int_gausslag = 0.;
+for ( int i = 1;  i <= n; i++){
+    int_gausslag += wgl[i]*sin(xgl[i]);
+}
+cout<< int_gausslag<<endl;
 }
