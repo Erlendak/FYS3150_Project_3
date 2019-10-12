19b10e3c15b01882175f450de927bcafc48ab8e5
erlendak <erlendak@mail.uio.no>
2019-10-12 19:42:56 +0200

    Started implementing Monte Carlo method.

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -25,24 +25,35 @@ int N = 20;
 
    //   set up the mesh points and weights
    //   set up the mesh points and weights and the power of x^alf
-double *xgl = new double [N+1];
-double *wgl = new double [N+1];
+
+double *x1 = new double [N];//Meshgrid for theta
+double *w1 = new double [N];// vekttall for theta
+double *x2 = new double [N];//Meshgrid for phi
+double *w2 = new double [N];//vekttall for phi
+int a = 0;
+double b = pi;
+gauleg(a,b,x1,w1, N);// For theta
+gauleg(a,2*b,x2,w2, N); // For phi
+double *xgl = new double [N+1]; // radiusens meshgrid
+double *wgl = new double [N+1]; // radiusen vekttall
 double alf = 2.0;
-gauss_laguerre(xgl,wgl, N, alf);
+gauss_laguerre(xgl,wgl, N, alf);//For radiusen
 
-vec _theta = linspace(0,pi,N+1);
-vec _phi =linspace(0,2*pi,N+1);
+//vec _theta = linspace(0,pi,N+1);
+//vec _phi =linspace(0,2*pi,N+1);
 double int_gausslag = 0.0;
-for (int i=1;i<=N;i++){
+for (int i=1;i<=N;i++){ //Radius 1
     cout << i << endl;
-    for (int j = 1; j<=N; j++){
-    for (int k = 1; k<=N; k++){
-    for (int l = 1; l<=N; l++){
-    for (int m = 1; m<=N; m++){
-    for (int n = 1; n<=N; n++){
+    for (int j = 1; j<=N; j++){//Radius 2
 
-    int_gausslag += wgl[i]*wgl[k]*wgl[m]*wgl[j]*wgl[l]*wgl[n]
-            *func_spherical(i,_theta(k),_phi(m),j,_theta(l),_phi(n));
+    for (int k = 0; k<N; k++){//Theta 1
+    for (int l = 0; l<N; l++){// Theta 2
+
+    for (int m = 0; m<N; m++){// Phi 1
+    for (int n = 0; n<N; n++){// Phi 2
+
+        int_gausslag += wgl[i]*wgl[j]*w1[k]*w1[l]*w2[m]*w2[n]
+            * func_spherical(xgl[i], x1[k], x2[m],xgl[j],x1[l],x2[n]);
 
 //cout<< int_gausslag<<endl;
 }}}}}}
