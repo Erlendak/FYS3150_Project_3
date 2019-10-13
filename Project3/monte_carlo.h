532d01c565d8a23f5132467171191b6c321fd8ae
erlendak <erlendak@mail.uio.no>
2019-10-13 18:02:14 +0200

    Added comments for for asking question.

--- Project3/monte_carlo.h
+++ Project3/monte_carlo.h
@@ -15,25 +15,23 @@ double brute_force_MC(double *);
 
 void brute(int n){
 
-     double a = 0;
+     double a = 0; // Kunne brukt indekser for forskjellige grense verdier.
      double b = 3;
      double jacobidet = 1;
      for ( int i = 0;  i < 6; i++){
-         jacobidet = jacobidet * (b- a) ;
+         jacobidet = jacobidet * (b- a) ; // \prod_{i=0}^{8} b_i - a_i
      }
 
      double x[6], y, fx;
      double int_mc = 1.;  double variance = 0.;
      double sum_sigma= 0. ; long idum=-1 ;
-     double length = 5.; // we fix the max size of the box to L=5
+     double length = 5.; // Gjelder for eksempel
 
-//   evaluate the integral with importance sampling
-     for ( int i = 1;  i <= n; i++){
-//   x[] contains the random numbers for all dimensions
-       for (int j = 0; j< 6; j++) {
-           x[j]=-length+2*length*ran0(&idum);
+     for ( int i = 1;  i <= n; i++){ // Monte Carlo integrasjons loopen
+       for (int j = 0; j< 6; j++) { // Setter opp virkårlige tall for dimensjonene
+           x[j]=-length+2*length*ran0(&idum); // Må skrives om for å passe hvor integrasjon?
        }
-       fx= func_cartesian(x[0],x[1],x[2],x[3],x[4],x[5]);
+       fx= func_cartesian(x[0],x[1],x[2],x[3],x[4],x[5]); // Kan vi bruke integranten fra tidligere?
        int_mc += fx;
        sum_sigma += fx*fx;
      }
@@ -42,8 +40,8 @@ void brute(int n){
      variance=sum_sigma-int_mc*int_mc;
 
 
-      cout <<  jacobidet*int_mc<< endl;
-      cout <<  sum_sigma<< endl;
+     cout <<  jacobidet*int_mc<< endl;
+     cout <<  sum_sigma<< endl;
      }
   // end of main program
 
