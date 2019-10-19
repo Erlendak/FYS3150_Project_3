a75a61abcbbbe76a92d22c9e1e17083ea42e2980
Erlendak <Erlendak@mail.uio.no>
2019-10-19 15:36:26 +0200

    Need to test old codes

---
+++ Local_DEV_project_3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <integrand.h>
+#include <methods.h>
+#include <cmath>
+#include <armadillo>
+#include <monte_carlo.h>
+#include <unit_tests.h>
+#include <omp.h> //The Open MPI package
+
+
+using namespace std;
+using namespace arma;
+
+    int main(){
+    //task3a();
+    //task3b();
+    //test_ans();
+    double integralening ;
+    double _std;
+    Importance_MonteCarlo(1000000, -2,2, integralening,_std);
+    cout <<integralening <<endl;
+    return 0;
+    }
